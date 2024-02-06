08e40462e1d824d26e0658eea7940d3de659c4e2
Carter <146026801+Carter425@users.noreply.github.com>
2024-02-06 02:07:32 -0800

    Delete AzureAIDragon directory

--- AzureAIDragon/lib/responseFormatter.d.ts
+++
@@ -1,6 +0,0 @@
-import { Application } from '@microsoft/teams-ai';
-/**
- *
- * @param {Application} app Application to add the response formatter to.
- */
-export declare function addResponseFormatter(app: Application): void;
--- AzureAIDragon/src/cards/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-// Additional adaptive cards may be added here for export. 
-
-export * from './staticCharacterCard';
